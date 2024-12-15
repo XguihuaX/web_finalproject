e2a36c3e09d7c73af5baf9ef97debae0b01de0f9
XguihuaX <101863062+XguihuaX@users.noreply.github.com>
2024-12-15 01:21:20 -0500

    Add files via upload

---
+++ note/backend/src/routes/indexRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// 首页路由
+router.get('/', (req, res) => {
+    res.render('index'); // 渲染 views 目录下的 index.ejs
+});
+
+module.exports = { indexRoutes: router };
