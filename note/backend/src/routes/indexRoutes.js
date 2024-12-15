831d9a685507c33c1d77fa500b4d7651e975296c
XguihuaX <101863062+XguihuaX@users.noreply.github.com>
2024-12-15 00:58:59 -0500

    Update indexRoutes.js

--- note/backend/src/routes/indexRoutes.js
+++ note/backend/src/routes/indexRoutes.js
@@ -1,19 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/", (_req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>欢迎来到我们的后端服务！</title>
-      </head>
-      <body>
-        <p>后端启动成功,如有需要，您可以通过API进行交互。</p>
-      </body>
-    </html>
-  `);
+// 首页路由
+router.get('/', (req, res) => {
+    res.render('index'); // 渲染 views 目录下的 index.ejs
 });
 
-module.exports = {
-  indexRoutes: router,
-};
+module.exports = { indexRoutes: router };
